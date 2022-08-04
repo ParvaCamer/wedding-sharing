0a5b09a7b490bc822d8255e93ec2c903100f7c43
ParvaCamer <64281030+ParvaCamer@users.noreply.github.com>
2022-08-04 12:59:59 +0200

    Update UserSignIn.vue

--- src/pages/authentification/UserSignIn.vue
+++ src/pages/authentification/UserSignIn.vue
@@ -3,5 +3,6 @@
         <h1>Weeding Sharing</h1>
         <input type="text" class="code" placeholder="Type your code">
         <router-link to="/signin">New weeding? Create new space</router-link>
+        <p>test</p>
     </div>
 </template>
